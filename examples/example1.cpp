3e155687fc1f927801a3a17ee8469b62181a0df5
ekildishev <ekildisev@gmail.com>
2019-04-15 17:48:35 +0300

    added sources

---
+++ examples/example1.cpp
@@ -0,0 +1,6 @@
+#include <print.hpp>
+
+int main(int argc, char** argv)
+{
+  print("hello");
+}
---
+++ examples/example2.cpp
@@ -0,0 +1,9 @@
+#include <print.hpp>
+
+#include <fstream>
+
+int main(int argc, char** argv)
+{
+  std::ofstream file("log.txt");
+  print(std::string("hello"), file);
+}
